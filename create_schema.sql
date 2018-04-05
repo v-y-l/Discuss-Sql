b7b2a3c3553d85ec42bf9438f99c8a46d10121ab
Victor Lin <vlin@nextjump.com>
2018-04-05 11:30:31 -0400

    Update tb_discuss_comment schema

--- create_schema.sql
+++ create_schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE tb_discuss_comment (
 	pk_discuss_comment_id bigint NOT NULL IDENTITY(1,1),
-	recipient_id bigint NOT NULL,
+	recipient_feedbackapp_response_id bigint NOT NULL,
 	sender_id bigint NOT NULL,
 	comment_text varchar(1000),
 	PRIMARY KEY (pk_discuss_comment_id),
-	FOREIGN KEY (recipient_id) REFERENCES tb_security_user (pk_security_userid),
+	FOREIGN KEY (recipient_feedbackapp_response_id) REFERENCES tb_feedbackapp_response (pk_feedbackapp_response_id),
 	FOREIGN KEY (sender_id) REFERENCES tb_security_user (pk_security_userid)
 )
 
