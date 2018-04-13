51fe3eed39237284dee38c1f109517e9723e6193
Victor Lin <vlin@nextjump.com>
2018-04-13 14:37:59 -0400

    Update tb_dicuss_followers schema

--- create_schema.sql
+++ create_schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE tb_discuss_comment (
 	pk_discuss_comment_id bigint NOT NULL IDENTITY(1,1),
-	recipient_feedbackapp_response_id bigint NOT NULL,
+	fk_pk_feedbackapp_response_id bigint NOT NULL,
 	sender_id bigint NOT NULL,
 	comment_text varchar(1000),
 	PRIMARY KEY (pk_discuss_comment_id),
-	FOREIGN KEY (recipient_feedbackapp_response_id) REFERENCES tb_feedbackapp_response (pk_feedbackapp_response_id),
+	FOREIGN KEY (fk_pk_feedbackapp_response_id) REFERENCES tb_feedbackapp_response (pk_feedbackapp_response_id),
 	FOREIGN KEY (sender_id) REFERENCES tb_security_user (pk_security_userid)
 )
 
@@ -18,10 +18,10 @@ CREATE TABLE tb_discuss_pseudonym (
 )
 
 CREATE TABLE tb_discuss_followers (
-	user_id bigint NOT NULL,
 	follower_id bigint NOT NULL,
+	followee_id bigint NOT NULL,
 	does_follow int NOT NULL ,
-	FOREIGN KEY (user_id) REFERENCES tb_security_user (pk_security_userid),
 	FOREIGN KEY (follower_id) REFERENCES tb_security_user (pk_security_userid),
-	PRIMARY KEY (user_id, follower_id)
+	FOREIGN KEY (followee_id) REFERENCES tb_security_user (pk_security_userid),
+	PRIMARY KEY (follower_id, followee_id)
 )
